9a43f185f299611b7e7c10e6f3882aa77d3f50e7
thatmanineededinmylife <93560358+thatmanineededinmylife@users.noreply.github.com>
2022-04-13 14:27:25 -0400

    Initial commit

---
+++ source/states/PlayState.hx
@@ -0,0 +1,40 @@
+package states;
+
+import achievements.Achievements;
+import flixel.addons.transition.FlxTransitionableState;
+import signals.Lifecycle;
+import entities.Player;
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+using extensions.FlxStateExt;
+
+class PlayState extends FlxTransitionableState {
+	var player:FlxSprite;
+
+	override public function create() {
+		super.create();
+		Lifecycle.startup.dispatch();
+
+		FlxG.camera.pixelPerfectRender = true;
+
+		player = new Player();
+		add(player);
+
+		add(Achievements.ACHIEVEMENT_NAME_HERE.toToast(true, true));
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+	}
+
+	override public function onFocusLost() {
+		super.onFocusLost();
+		this.handleFocusLost();
+	}
+
+	override public function onFocus() {
+		super.onFocus();
+		this.handleFocus();
+	}
+}
